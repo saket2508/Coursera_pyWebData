8e0fed852196808f5c5825af9256094917019be3
Saket S Iyer <52590360+saket2508@users.noreply.github.com>
2019-09-23 19:14:33 +0530

    Update regex.py

--- regex.py
+++ regex.py
@@ -1,9 +1,13 @@
 import re
-textf = open('actualData.txt','r')
-lines = textf.readlines()
-res = list()
-for line in lines:
-    prs = re.findall('[0-9]+',line)
-    res.extend(prs)
-res = list(map(int,res))
-print(sum(res))
+def parse():
+    textf = open('actualData.txt','r')
+    lines = textf.readlines()
+    res = list()
+    for line in lines:
+        prs = re.findall('[0-9]+',line)
+        res.extend(prs)
+    res = list(map(int,res))
+    print(sum(res))
+
+if __name__== '__main__':
+    parse()
