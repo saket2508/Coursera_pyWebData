938c0f54c194f184cef80af860a81b08791ae396
Saket S Iyer <52590360+saket2508@users.noreply.github.com>
2019-09-23 19:14:00 +0530

    Update socket1.py

--- socket1.py
+++ socket1.py
@@ -1,14 +1,17 @@
 import socket
+def getData():
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.connect(('data.pr4e.org', 80))
+    cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+    mysock.send(cmd)
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1:
+            break
+        print(data.decode(),end='')
+    mysock.close()
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(),end='')
+if __name__== '__main__':
+    getData()
 
-mysock.close()
